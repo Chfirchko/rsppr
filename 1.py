5f713a7c0007af97e59fce25283cfbbbf8828b56
Chfirchko <anatol30@list.ru>
2023-04-11 01:16:10 +0300

    1 ржака 3.0

--- 1.py
+++ 1.py
@@ -3,6 +3,7 @@ import random
 import pandas as pd
 import openpyxl
 import string
+import numpy as np
 
 
 class agent:
@@ -22,6 +23,8 @@ def func(people):
             rus += 1
         else:
             hohl += 1
+    if rus == 0 or hohl == 0:
+        return 1
     rus_dol_year = 11600
     ucr_dol_year = 2724
     T = random.uniform(0, 0.5)
@@ -70,13 +73,30 @@ def srach_1(x, y, qwerty, intense):
         x.pts += abs(x.position) + 0.01 * intense
         y.pts += abs(y.position) + 0.01 * intense
         hohlosrach.write('123321\n')
-    elif float(x.knowledge.cell(row=num, column=3).value) + x.pts > float(y.knowledge.cell(row=num, column=3).value) + y.pts:
+    elif float(x.knowledge.cell(row=num, column=3).value) + x.pts > float(
+            y.knowledge.cell(row=num, column=3).value) + y.pts:
         x.pts += abs(x.position) + 0.01 * intense
         hohlosrach.write('1\n')
-    elif float(x.knowledge.cell(row=num, column=3).value) + x.pts < float(y.knowledge.cell(row=num, column=3).value) + y.pts:
+    elif float(x.knowledge.cell(row=num, column=3).value) + x.pts < float(
+            y.knowledge.cell(row=num, column=3).value) + y.pts:
         y.pts += abs(y.position) + 0.01 * intense
         hohlosrach.write('2\n')
-    return wiener
+    if x.pts > 100 and x.position >= 0:
+        stroka = 'Хохла порвало\n'
+        hohlosrach.write(stroka)
+        return 1
+    elif y.pts > 100 and x.position >= 0:
+        stroka = 'Хохла порвало\n'
+        hohlosrach.write(stroka)
+        return 1
+    elif x.pts > 100 and x.position < 0:
+        stroka = 'Слава Хохляндии\n'
+        hohlosrach.write(stroka)
+        return 2
+    elif y.pts > 100 and x.position < 0:
+        stroka = 'Слава Хохляндии\n'
+        hohlosrach.write(stroka)
+        return 2
 
 
 def srach(x, y, qwerty, hohlosrach, intense):
@@ -116,7 +136,7 @@ hohlin = hohli.active
 
 russkie = 0
 
-for i in range(0, 100):
+for i in range(0, 2000):
     pos = random.uniform(-10, 10)
     if 0 <= pos:
         ag = agent(pos, russians, 1, False, 1)
@@ -125,12 +145,33 @@ for i in range(0, 100):
     elif pos < 0:
         ag = agent(pos, hohlin, 2, False, 1)
         people.append(ag)
-people.sort(key=lambda x: x.type)
-for i in people:
-    print(i.type)
-intense = func(people)
+    # people.sort(key=lambda x: x.type)
 
-for srachi in range(0, 10):
+rus = 0
+hohl = 0
+for i in people:
+    if i.type == 1:
+        rus += 1
+    else:
+        hohl += 1
+print('Русские:', rus)
+print('Хохлы', hohl)
+for srachi in range(0, 50):
+    intense = func(people)
+    if intense == 1:
+        break
+    if intense > 15:
+        for i in range(2, russians.max_row):
+            if russians.cell(row=i, column=4).value == 0:
+                russians.cell(row=i, column=4).value = 1
+                break
+    if intense > 17:
+        for i in range(2, hohlin.max_row):
+            if hohlin.cell(row=i, column=4).value == 0:
+                hohlin.cell(row=i, column=4).value = 1
+                break
+
+    np.random.shuffle(people)
     j = len(people) - 1
     stroka = '+++++++++++++++}{0}{0срач №' + str(srachi) + '+++++++++++++++\n'
     hohlosrach.write(stroka)
@@ -138,10 +179,43 @@ for srachi in range(0, 10):
         if j < i:
             break
         if people[i].spor == False and people[j].spor == False:
-            srach(people[i], people[j], i, hohlosrach, intense)
+            wiener = srach(people[i], people[j], i, hohlosrach, intense)
             j -= 1
+            if wiener == 1 and people[i].position >= 0:
+                #people.pop(j)
+                people[j].position = abs(people[j].position)
+                people[j].type = 1
+                people[j].knowledge = russians
+            elif wiener == 1 and people[j].position >= 0:
+                #people.pop(i)
+                people[i].position = abs(people[i].position)
+                people[i].type = 1
+                people[i].knowledge = russians
+            elif wiener == 2 and people[i].position >= 0:
+                #people.pop(j)
+                people[j].position = -1 * people[j].position
+                people[j].type = 2
+                people[j].knowledge = hohlin
+            elif wiener == 2 and people[j].position >= 0:
+                #people.pop(i)
+                people[i].position = -1 * people[i].position
+                people[i].type = 2
+                people[i].knowledge = hohlin
+
     for asd in people:
         asd.spor = False
-print('Интенсивность срача: ', intense, '\n')
+    print('Интенсивность срача: ', intense, '\n')
+zxc = 0
 for i in people:
-    print('Скил срача', i.pts)
+    zxc += 1
+    print(zxc, 'Кто по масти:', i.type, 'Скил срача', i.pts)
+
+rus = 0
+hohl = 0
+for i in people:
+    if i.type == 1:
+        rus += 1
+    else:
+        hohl += 1
+print('Русские:', rus)
+print('Хохлы', hohl)
